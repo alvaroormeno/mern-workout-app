4dc97cf688f74278f14d2c3b5cea74f3e71ef82a
alvaroormeno <alvaro@ormeno.org>
2022-07-10 23:28:04 -0400

    saving jic

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,14 @@
 const express = require('express')
 
-//Create express app, saves express function in app
+//Create express app, saves express function in app constant
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({message: "Welcome to the app!"})
+
+})
+
+// listen for requests
+app.listen(4000, () => {
+    console.log('listening on port 4000')
+})
