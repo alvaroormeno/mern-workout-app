49587e96ad859d002a3caf6014315cef7a7ec519
alvaroormeno <alvaro@ormeno.org>
2022-07-12 16:34:17 -0400

    setup middleware

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
 
 //Create express app, saves express function in app constant
 const app = express();
 
+//MIDDLEWARE
+// note: every time a request comes in, this function will fire.      
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
 app.get('/', (req, res) => {
     res.json({message: "Welcome to the app!"})
 
 })
 
 // listen for requests
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log('listening on port 4000')
 })
+
