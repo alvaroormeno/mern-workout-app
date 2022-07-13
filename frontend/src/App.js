2678aceb79956c0d11cb967315618124008e5af1
alvaroormeno <alvaro@ormeno.org>
2022-07-13 12:30:59 -0400

    index css and navbar component completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+//Import Pages
+import Home from './pages/Home';
+//Import Components
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Navbar/>
+      <div className="pages">
+        <Routes>
+          <Route
+            path="/"
+            element={<Home/>}
+            />
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
